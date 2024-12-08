1f74dec94d1decf37af751bd6b6a8748e6214c5a
bitbaso <bitbaso@gmail.com>
2024-12-08 16:08:43 +0100

    Footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
 	import '../app.css';
 	import Navbar from '$lib/components/Navbar.svelte'; // Importa el componente de Navbar
+	import Footer from '$lib/components/Footer.svelte'; // Importa el componente de Footer
 	let { children } = $props();
 </script>
 
 <Navbar />
   {@render children()}
+<Footer />
