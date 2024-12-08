62d6ed1d9d2449700ccc92caf9fcc52a3224221c
bitbaso <bitbaso@gmail.com>
2024-12-08 13:49:47 +0100

    design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -120,7 +120,7 @@
 <canvas bind:this={canvas} class="bg-gradient-to-r from-stone-900 to-slate-200"></canvas>
 
 <!-- Contenedor de texto con posición relativa, que se coloca sobre el canvas -->
-<div class="relative z-10 flex flex-col items-center justify-center pt-16 mt-5 pb-2">
+<div class="relative z-10 flex flex-col items-center justify-center pt-16 mt-5 pb-6">
   <div class="flex items-center justify-center">
     <div class="rounded-xl bg-gradient-to-b from-green-600 to-slate-700 pr-5 pl-5 pt-1 pb-1 font-bold text-6xl text-white">b</div>
     <h1 class="text-6xl font-bold text-white drop-shadow-lg">itbaso</h1>
